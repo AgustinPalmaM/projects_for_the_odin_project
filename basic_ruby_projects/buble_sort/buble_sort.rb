635cfe921b723d8a5f0b5a7fd1d587041614fbe0
AgustinPalmaM <palma.agustin7@gmail.com>
2022-04-27 19:35:48 -0400

    improve the way of make the swap

--- basic_ruby_projects/buble_sort/buble_sort.rb
+++ basic_ruby_projects/buble_sort/buble_sort.rb
@@ -1,17 +1,13 @@
 def bubble_sort(arr)
   arr.each do |n|
     arr.each_with_index do |num, index|
-      unless arr[index + 1] == nil
+      unless arr[index + 1].nil?
         if arr[index] > arr[index + 1]
-          a = arr[index]
-          b = arr[index + 1]
-          arr[index] = b
-          arr[index + 1] = a
+          arr[index], arr[index + 1] = arr[index + 1], arr[index]
         end
       end
     end
   end
-  arr
 end
 
 puts bubble_sort([4, 3, 78, 2, 0, -1])
